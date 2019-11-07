9551f6a386a588315974a09615df688da02dc998
98Glopes <gabriel.lope@outlook.com>
2019-11-06 21:18:50 -0300

    add ex10

--- ex10/banco_dados.c
+++ ex10/banco_dados.c
@@ -1,10 +1,118 @@
 #include"banco_dados.h"
+#include<stdlib.h>
+#include<stdio.h>
+
+void inicializa_banco(void){
+
+    MeuBanco = malloc(tamanho_banco*sizeof(Registro));
+
+    for(int i=0; i < tamanho_banco; i++){
+
+        (MeuBanco+i)->preenchido = 0;
+        
+    }
+}
+
+void grava_info(Registro NovoRegistro){
+
+    if(verifica_memoria() == 0){
+        aloca_memoria();
+    }
+    for(int i=0; i < tamanho_banco; i++){
+
+        if((MeuBanco+i)->preenchido == 0){
+
+            (MeuBanco+i)->primary_key = primary_key;
+            (MeuBanco+i)->preenchido = 1;
+            (MeuBanco+i)->propriedade = NovoRegistro.propriedade;
+
+            primary_key++;
+            break;
+
+        }
+        
+    }
+}
+
+void exibe_tudo(void){
+
+    for(int i=0; i < tamanho_banco; i++){
+
+        if( (MeuBanco+i)->preenchido == 1){
+
+            print_registro(*(MeuBanco+i));
+
+        }
+
+    }
+
+}
+
+void print_registro(Registro NovoRegistro){
+
+    printf("Chave Primaria: %i\n", NovoRegistro.primary_key);
+    printf("informacao: %i\n\n", NovoRegistro.propriedade);
+}
+
+void deleta_registro(unsigned int primary_key){
+
+    for(int i = 0; i < tamanho_banco; i++){
+
+        if( (MeuBanco+i)->primary_key == primary_key){
+
+            (MeuBanco+i)->preenchido = 0;
+        }         
+    }
+
+}
+
+void aloca_memoria(void){
+
+    MeuBanco = realloc(MeuBanco, tamanho_banco+10);
+
+    for(int i = tamanho_banco;
+            i < tamanho_banco+10; i++){
+
+        (MeuBanco+i)->preenchido = 0;
+
+    }
+    tamanho_banco+=10;
+
+}
+
+int verifica_memoria(void){
+    
+    int espaco_livre = 0;
+    for(int i=0; i < tamanho_banco; i++){
+
+        if ((MeuBanco+i)->preenchido == 0)
+            espaco_livre++;
+
+    }
+    return espaco_livre;
+}
+
+Registro exibe_registro(unsigned int indice){
+
+    if(indice > tamanho_banco){
+        return *MeuBanco;
+    }
+    return *(MeuBanco+indice);
+}
 
 
-struct Registro reg;
+void main(void){
 
-sizeof Registro
-void main(void){    
+    Registro gravar;
+    gravar.propriedade = 19;
+
+    inicializa_banco();
 
 
+    for(int i=0;i<50;i++){
+        grava_info(gravar);
+    }
+
+
+    exibe_tudo();
 }
--- ex10/banco_dados.h
+++ ex10/banco_dados.h
@@ -1,11 +1,26 @@
 //exercicio 10
 // header file para função do banco de dados
+#pragma once
 
+static int tamanho_banco = 20;
+static int primary_key = 0;
 
-struct Registro{
+//esta struct representa um item a ser cadastrado
+typedef struct  {
+	unsigned int primary_key;
+	int preenchido;
+	int propriedade;
+	
+}Registro;
 
-    char preenchido;
-    char nome;
+//ponteiro para o vetor de Registro para alocação dinamica
+static Registro *MeuBanco;
 
-};
-
+void inicializa_banco(void);
+void grava_info(Registro NovoRegistro);
+void exibe_tudo(void);
+void print_registro(Registro NovoRegistro);
+void deleta_registro(unsigned int primary_key);
+void aloca_memoria(void);
+int verifica_memoria(void);
+Registro exibe_registro(unsigned int primary_key);
